a71e3da6dc761ecbe73409b5f6e9e3866752b2df
Paulo Henrique <paulotux.mendes@gmail.com>
2019-06-16 22:50:04 -0300

    [UPDATE] Ajuste nas telas de produtos

--- resources/views/pedidos/index.blade.php
+++ resources/views/pedidos/index.blade.php
@@ -2,10 +2,7 @@
 
 @section('content')
     <section class="content-header">
-        <h1 class="pull-left">Pedidos</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('pedidos.create') !!}">Add New</a>
-        </h1>
+        <h1 class="pull-left" style="margin-top: 20px;margin-bottom: 15px" >Pedidos</h1>
     </section>
     <div class="content">
         <div class="clearfix"></div>
@@ -21,6 +18,10 @@
         <div class="text-center">
         
         </div>
+
+        <h1 class="pull-right">
+            <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('pedidos.create') !!}">Cadastrar Pedido</a>
+        </h1>
     </div>
 @endsection
 
